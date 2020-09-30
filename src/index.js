21dd4a9b732ca9caa39d16d30a711188863f78be
etsadhakal <etsadhakal@icloud.com>
2020-09-30 22:56:13 +0545

    reset password changes and fixes on gif

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import 'bootstrap';
 import $ from "jquery";
-
-
-import html from './reset.html';
-// import ('./reset.html');
-
 import 'remixicon/fonts/remixicon.css';
 
+import html from './reset.html';
+
 import imageURL from './assets/ndrmaLogo-nepal.svg';
+import image from './assets/mailbox.gif';
 
 // JQuery
 $(document).ready(() => {
