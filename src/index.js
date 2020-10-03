8e47c7d3dc0098028c3880d082108f882f3a4a17
etsadhakal <etsadhakal@icloud.com>
2020-10-03 14:19:57 +0545

    dashboard page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import 'bootstrap';
 import $ from "jquery";
+import Chart from 'chart.js';
+
+
+
 
 import('./reset-password.html');
 import('./forgot.html');
@@ -27,4 +31,91 @@ $(document).ready(function () {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
+});
+
+//charts
+var ctx = document.getElementById('stockChart').getContext("2d");
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['State 1', 'State 2', 'Gandaki', 'Bagmati', 'State 5', 'Karnali', 'SudurPaschim'],
+        datasets: [{
+            label: 'Stocks',
+            data: [2343, 2641, 1672, 1878, 2092, 2012, 1198],
+            backgroundColor: [
+                '#eaeaea',
+                '#eaeaea',
+                '#eaeaea',
+                '#eaeaea',
+                '#eaeaea',
+                '#eaeaea',
+                '#eaeaea'
+            ],
+            hoverBackgroundColor: [
+                '#007bff',
+                '#007bff',
+                '#007bff',
+                '#007bff',
+                '#007bff',
+                '#007bff',
+                '#007bff',
+            ],
+            borderWidth: 0,
+
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    display: false
+                },
+                gridLines: {
+                    display: false,
+                    drawBorder: false //<- set this
+                },
+            }],
+            xAxes: [{
+                ticks: {
+                    display: false
+                },
+                gridLines: {
+                    display: false,
+                    drawBorder: false //<- set this
+                },
+
+            }]
+        },
+        legend: {
+            display: false
+        },
+        layout: {
+            padding: {
+                left: 0,
+                right: 0,
+                top: 10,
+                bottom: 0
+            }
+        },
+        cornerRadius: 4,
+        tooltips: {
+            callbacks: {
+                labelColor: function (tooltipItem, chart) {
+                    return {
+                        borderColor: '#333',
+                        backgroundColor: '#333',
+                        cornerRadius: 1,
+                    };
+                },
+                labelTextColor: function (tooltipItem, chart) {
+                    return '#fff';
+                }
+            }
+        },
+        hover: {
+            onHover: function (e) {
+                $("#stockChart").css("cursor", e[0] ? "pointer" : "default");
+            }
+        }
+    }
 });
